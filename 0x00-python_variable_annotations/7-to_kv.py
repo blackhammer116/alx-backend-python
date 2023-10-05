ef622d52f49625e0ae8de46c2508adaab458521a
Nebiyu <nebiyuhiyabu@gmail.com>
2023-10-05 19:29:27 +0000

    version annotation fix

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 """
 Optional module for the function argument
+Tuple to annotate the return type of the function
 """
-from typing import Union
+from typing import Union, Tuple
 
 
-def to_kv(k: str, v: Union[int, float]) -> tuple[str, float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """
     A function that takes a string and [int | float] then returns
     a tuple containing the string and the square of the float
