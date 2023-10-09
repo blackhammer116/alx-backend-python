b6b6b41bb1c2740ff1d4be928eb5869bff238694
Nebiyu <nebiyuhiyabu@gmail.com>
2023-10-09 15:27:49 +0300

    ensuring future via asyncio to run concurrent tasks

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -12,7 +12,7 @@ import asyncio
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> list[float]:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """
     A function that returns the delay time
     of wait_random 'n' times
