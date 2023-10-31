42637cb4f1de1172fc5b2efc93c2d84aff2b0b62
Nebiyu <nebiyuhiyabu@gmail.com>
2023-10-31 08:29:18 +0000

    test_memoize

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -7,7 +7,7 @@ import requests
 from parameterized import parameterized
 from unittest import TestCase
 from unittest import mock
-from utils import access_nested_map, get_json
+from utils import access_nested_map, get_json, memoize
 from typing import (
     Mapping,
     Sequence,
@@ -81,3 +81,33 @@ class TestGetJson(TestCase):
             mock_get.assert_called_once_with(test_url)
             self.assertEqual(result, test_payload)
             mock_get.reset_mock()
+
+
+class TestMemoize(TestCase):
+    """
+    A class to test memoize decorator
+    Args:
+        TestCase: a test case inherited from unittest
+    """
+    def test_memoize(self):
+        """
+        A method to test memoize decorator
+        """
+        class TestClass:
+            """
+            A test class to test the methods return value
+            """
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        test_instance = TestClass()
+        with mock.patch.object(test_instance, 'a_method') as mock_a_method:
+            mock_a_method.return_value = mock.Mock(return_value=42)
+            result1 = test_instance.a_property()
+            result2 = test_instance.a_property()
+            mock_a_method.assert_called_once()
+            self.assertEqual(result1, result2)
